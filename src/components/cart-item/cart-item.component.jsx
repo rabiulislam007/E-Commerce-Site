ff647d32d2cbb66f6e3c5d02cbced58c568c9daf
Rabi Ul Islam <rabiulislam9@gmail.com>
2022-09-13 23:15:30 +0600

    added in add to cart features

---
+++ src/components/cart-item/cart-item.component.jsx
@@ -0,0 +1,16 @@
+import './cart-item.styles.scss';
+
+const CartItem = ({ cartItem }) => {
+  const { name, imageUrl, price, quantity } = cartItem;
+  return (
+    <div className='cart-item-container'>
+      <img src={imageUrl} alt={`${name}`} />
+      <span className='name'>{name}</span>
+      <span className='price'>
+        {quantity} * ${price}
+      </span>
+    </div>
+  );
+};
+
+export default CartItem;
